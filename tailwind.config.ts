71d5eaa76a64e9c2772cf79cc7ff1a6916fb22f2
zackgakuo <zachary.gakuo@fin.africa>
2023-12-15 13:21:15 +0300

    initialize mobile template app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,19 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'main-background': "url('/backgrounds/rm222-mind-30.jpg')"
       },
+      transitionProperty: {
+        height: "height",
+        "max-height": "max-height",
+      },
+      animation: {
+        bounce200: "bounce 1s infinite 200ms",
+        bounce400: "bounce 1s infinite 400ms",
+      },
+      boxShadow:{
+        'card': '0 3px 10px rgb(0,0,0,0.2)',
+      }
     },
   },
   plugins: [],
